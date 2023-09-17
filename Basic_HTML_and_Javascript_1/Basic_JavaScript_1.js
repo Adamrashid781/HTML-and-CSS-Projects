4a03a1720fec61343d3b20c3bda9c45dd3b0df44
Adamrashid781 <adamrashid781@gmail.com>
2023-09-16 18:15:24 -0700

    Submitting Assignment

--- Basic_HTML_and_Javascript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_Javascript_1/Basic_JavaScript_1.js
@@ -1,1 +1,8 @@
 alert("This is from the JS file!")
+
+
+function My_First_Function(){
+    var string = "Kiss me, I'm Irish!";
+    var result = string.fontcolor("red");
+    document.getElementById("Irish").innerHTML = result;
+}
